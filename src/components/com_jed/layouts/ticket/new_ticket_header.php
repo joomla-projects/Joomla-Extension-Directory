084d5eae8c980a1aa5897e8a9b5460b6f7bb5aed
Hannes Papenberg <info@joomlager.de>
2025-05-05 10:37:37 +0200

    Update CI setup

--- src/components/com_jed/layouts/ticket/new_ticket_header.php
+++ src/components/com_jed/layouts/ticket/new_ticket_header.php
@@ -24,7 +24,7 @@
 <div id="ticket-guidelines">
     <?php
     if ($displayData->ticket_title == "Submit Ticket") { ?>
-    <a><b>This support system is primarily for developers that have listings on this site. If you need support with a specific extension please contact the developer or try the Joomla Forum. </b>If you have found an extension that no longer exists, please use the <a href="index.php?option=com_jed&view=velabandonedreportform">VEL abandoned report</a>.</p>
+        <p><b>This support system is primarily for developers that have listings on this site. If you need support with a specific extension please contact the developer or try the Joomla Forum. </b>If you have found an extension that no longer exists, please use the <a href="index.php?option=com_jed&view=velabandonedreportform">VEL abandoned report</a>.</p>
         <p>To submit a new extension, always <span style="color: #ff0000;"><strong>check</strong></span> your extensions with <a href="http://extensions.joomla.org/extension/jedchecker" target="_blank" rel="noopener noreferrer">JED Checker</a> and then use <a href="index.php?option=com_jed&view=extensionform">this link</a> . Extensions with errors are rejected.</p>
         <p>For extension support, including how to use it or reporting a bug, please contact the extension developer directly.</p>
 
