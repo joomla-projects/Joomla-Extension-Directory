c58253ab94d60b75bf186ac000595e7177f7e9d6
Hannes Papenberg <info@joomlager.de>
2026-07-15 19:44:32 +0200

    Extract VEL management into a standalone com_vel

---
+++ src/administrator/components/com_vel/src/Controller/DeveloperupdateController.php
@@ -0,0 +1,35 @@
+<?php
+
+/**
+ * @package VEL
+ *
+ * @subpackage VEL
+ *
+ * @copyright (C) 2006-2026 Open Source Matters, Inc. <https://www.joomla.org>
+ * @license   GNU General Public License version 2 or later; see LICENSE.txt
+ */
+
+namespace Jed\Component\Vel\Administrator\Controller;
+
+// phpcs:disable PSR1.Files.SideEffects
+\defined('_JEXEC') or die;
+// phpcs:enable PSR1.Files.SideEffects
+
+use Joomla\CMS\MVC\Controller\FormController;
+
+/**
+ * Content article class.
+ *
+ * @since 4.0.0
+ */
+class DeveloperupdateController extends FormController
+{
+    /**
+     * A string showing the plural of the current object
+     *
+     * @var string
+     *
+     * @since 4.0.0
+     */
+    protected $view_list = 'Developerupdates';
+}
