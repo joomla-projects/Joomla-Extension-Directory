6a1715baae5e382f432ada9699813a3982c70c48
Hannes Papenberg <info@joomlager.de>
2026-08-09 10:24:56 +0200

    Fix sampledata plugin

--- src/plugins/sampledata/jed/jed.php
+++ src/plugins/sampledata/jed/jed.php
@@ -15,9 +15,12 @@ use Joomla\CMS\Table\Asset;
 use Joomla\CMS\Application\AdministratorApplication;
 use Joomla\CMS\Application\ApplicationHelper;
 use Joomla\CMS\Component\ComponentHelper;
+use Joomla\CMS\Event\Plugin\AjaxEvent;
+use Joomla\CMS\Event\SampleData\GetOverviewEvent;
 use Joomla\CMS\Factory;
 use Joomla\CMS\Language\Text;
 use Joomla\CMS\Plugin\CMSPlugin;
+use Joomla\CMS\User\UserFactoryInterface;
 use Joomla\Component\Categories\Administrator\Model\CategoryModel;
 use Joomla\Database\DatabaseDriver;
 
@@ -30,7 +33,7 @@ use Joomla\Database\DatabaseDriver;
  *
  * @since  4.0.0
  */
-class PlgSampledataJed extends CMSPlugin
+class PlgSampledataJed extends CMSPlugin implements SubscriberInterface
 {
     use DatabaseAwareTrait;
 
@@ -60,13 +63,35 @@ class PlgSampledataJed extends CMSPlugin
     }
 
     /**
+     * Returns an array of events this subscriber will listen to.
+     *
+     * @return  array
+     *
+     * @since   4.0.0
+     */
+    public static function getSubscribedEvents(): array
+    {
+        return [
+            'onSampledataGetOverview'    => 'onSampledataGetOverview',
+            'onAjaxSampledataApplyStep1' => 'onAjaxSampledataApplyStep1',
+            'onAjaxSampledataApplyStep2' => 'onAjaxSampledataApplyStep2',
+            'onAjaxSampledataApplyStep3' => 'onAjaxSampledataApplyStep3',
+            'onAjaxSampledataApplyStep4' => 'onAjaxSampledataApplyStep4',
+            'onAjaxSampledataApplyStep5' => 'onAjaxSampledataApplyStep5',
+            'onAjaxSampledataApplyStep6' => 'onAjaxSampledataApplyStep6',
+        ];
+    }
+
+    /**
      * Get an overview of the proposed sampledata.
      *
-     * @return  object  Object containing the name, title, description, icon and steps.
+     * @param   GetOverviewEvent  $event  Event instance.
+     *
+     * @return  void
      *
      * @since  3.8.0
      */
-    public function onSampledataGetOverview()
+    public function onSampledataGetOverview(GetOverviewEvent $event): void
     {
         $data              = new stdClass();
         $data->name        = $this->_name;
@@ -75,17 +100,19 @@ class PlgSampledataJed extends CMSPlugin
         $data->icon        = 'money';
         $data->steps       = 6;
 
-        return $data;
+        $event->addResult($data);
     }
 
     /**
      * Make sure we don't overwrite current admin user, move them to user_id=5
      *
-     * @return  array|void  Will be converted into the JSON response to the module.
+     * @param   AjaxEvent  $event  Event instance.
+     *
+     * @return  void
      *
      * @since  3.8.0
      */
-    public function onAjaxSampledataApplyStep1()
+    public function onAjaxSampledataApplyStep1(AjaxEvent $event): void
     {
         if ($this->getApplication()->getInput()->get('type') !== $this->_name) {
             return;
@@ -97,7 +124,7 @@ class PlgSampledataJed extends CMSPlugin
         $response['success'] = true;
         $response['message'] = Text::_('PLG_SAMPLEDATA_JED_STEP1_SUCCESS');
 
-        return $response;
+        $event->addResult($response);
     }
 
     /**
@@ -114,10 +141,12 @@ class PlgSampledataJed extends CMSPlugin
      */
     private function moveCurrentUserToId5(): void
     {
+        $app           = $this->getApplication();
         $db            = $this->getDatabase();
-        $currentUserId = (int) $this->getApplication()->getIdentity()->id;
+        $identity      = $app->getIdentity();
+        $currentUserId = (int) $identity->id;
 
-        if ($currentUserId === 5) {
+        if ($currentUserId === 5 || $currentUserId < 1) {
             return;
         }
 
@@ -133,54 +162,90 @@ class PlgSampledataJed extends CMSPlugin
 
         $db->setQuery('UPDATE #__user_usergroup_map SET user_id = 5 WHERE user_id = ' . $currentUserId);
         $db->execute();
+
+        $this->followRenumberedUserInSession($currentUserId);
+    }
+
+    /**
+     * The session only stores the user id (see User::__sleep()), so after renumbering the account
+     * User::__wakeup() no longer finds the old id on the next request and silently degrades the
+     * identity to a guest. In the backend that turns every following wizard step into an
+     * unauthorized request, so step 2 already fails before it starts. Point the running session at
+     * the new id instead.
+     *
+     * @param   int  $previousUserId  The id the current user had before being moved to 5.
+     *
+     * @return  void
+     *
+     * @since   4.0.0
+     */
+    private function followRenumberedUserInSession(int $previousUserId): void
+    {
+        $app     = $this->getApplication();
+        $db      = $this->getDatabase();
+        $session = $app->getSession();
+
+        $user = Factory::getContainer()->get(UserFactoryInterface::class)->loadUserById(5);
+
+        $session->set('user', $user);
+        $app->loadIdentity($user);
+
+        // The session metadata row is keyed on the old id as well.
+        $db->setQuery('UPDATE #__session SET userid = 5 WHERE userid = ' . $previousUserId);
+        $db->execute();
     }
 
     /**
      * First step to enter the sampledata. Tags
      *
-     * @return  array|void  Will be converted into the JSON response to the module.
+     * @param   AjaxEvent  $event  Event instance.
+     *
+     * @return  void
      *
      * @since  3.8.0
      */
-    public function onAjaxSampledataApplyStep2()
+    public function onAjaxSampledataApplyStep2(AjaxEvent $event): void
     {
         if ($this->getApplication()->getInput()->get('type') !== $this->_name) {
             return;
         }
 
         $this->importFile(__DIR__ . '/sql/step2.sql');
+        $db = $this->getDatabase();
 
-        $this->getDatabase()->setQuery('SELECT id FROM #__assets WHERE name = \'com_jed\'');
-        $component_asset_id = $this->getDatabase()->loadResult();
-        $this->getDatabase()->setQuery('DELETE FROM `#__assets` WHERE parent_id = \'' . $component_asset_id . '\';');
-        $this->getDatabase()->execute();
-        $this->getDatabase()->setQuery('INSERT INTO `#__assets` (parent_id, LEVEL, NAME, title, rules) SELECT ' . $component_asset_id . ' AS parent_id, level + 1 AS LEVEL, CONCAT(\'com_jed.category.\', id) AS NAME, title, \'{}\' AS rules FROM `#__categories` WHERE extension = \'com_jed\'');
-        $this->getDatabase()->execute();
+        $db->setQuery('SELECT id FROM #__assets WHERE name = \'com_jed\'');
+        $component_asset_id = $db->loadResult();
+        $db->setQuery('DELETE FROM `#__assets` WHERE parent_id = \'' . $component_asset_id . '\';');
+        $db->execute();
+        $db->setQuery('INSERT INTO `#__assets` (parent_id, LEVEL, NAME, title, rules) SELECT ' . $component_asset_id . ' AS parent_id, level + 1 AS LEVEL, CONCAT(\'com_jed.category.\', id) AS NAME, title, \'{}\' AS rules FROM `#__categories` WHERE extension = \'com_jed\'');
+        $db->execute();
 
-        $this->getDatabase()->setQuery('SELECT id FROM #__assets WHERE name = \'com_jed.category.9\'');
-        $asset_id = $this->getDatabase()->loadResult();
+        $db->setQuery('SELECT id FROM #__assets WHERE name = \'com_jed.category.9\'');
+        $asset_id = $db->loadResult();
 
-        $this->getDatabase()->setQuery('UPDATE #__categories SET asset_id = id + ' . ($asset_id - 8) . ' WHERE id > 8');
-        $this->getDatabase()->execute();
+        $db->setQuery('UPDATE #__categories SET asset_id = id + ' . ($asset_id - 8) . ' WHERE id > 8');
+        $db->execute();
 
-        $table = new Asset($this->getDatabase(), $this->getDispatcher());
+        $table = new Asset($db, $this->getDispatcher());
         $table->rebuild();
 
         $response            = [];
         $response['success'] = true;
         $response['message'] = Text::_('PLG_SAMPLEDATA_JED_STEP2_SUCCESS');
 
-        return $response;
+        $event->addResult($response);
     }
 
     /**
      * Second step to enter the sampledata. Banners
      *
-     * @return  array|void  Will be converted into the JSON response to the module.
+     * @param   AjaxEvent  $event  Event instance.
+     *
+     * @return  void
      *
      * @since  3.8.0
      */
-    public function onAjaxSampledataApplyStep3()
+    public function onAjaxSampledataApplyStep3(AjaxEvent $event): void
     {
         if ($this->getApplication()->getInput()->get('type') !== $this->_name) {
             return;
@@ -192,17 +257,19 @@ class PlgSampledataJed extends CMSPlugin
         $response['success'] = true;
         $response['message'] = Text::_('PLG_SAMPLEDATA_JED_STEP3_SUCCESS');
 
-        return $response;
+        $event->addResult($response);
     }
 
     /**
      * Third step to enter the sampledata. Content 1/2
      *
-     * @return  array|void  Will be converted into the JSON response to the module.
+     * @param   AjaxEvent  $event  Event instance.
+     *
+     * @return  void
      *
      * @since  3.8.0
      */
-    public function onAjaxSampledataApplyStep4()
+    public function onAjaxSampledataApplyStep4(AjaxEvent $event): void
     {
         if ($this->getApplication()->getInput()->get('type') !== $this->_name) {
             return;
@@ -214,17 +281,19 @@ class PlgSampledataJed extends CMSPlugin
         $response['success'] = true;
         $response['message'] = Text::_('PLG_SAMPLEDATA_JED_STEP4_SUCCESS');
 
-        return $response;
+        $event->addResult($response);
     }
 
     /**
      * Fourth step to enter the sampledata. Content 2/2
      *
-     * @return  array|void  Will be converted into the JSON response to the module.
+     * @param   AjaxEvent  $event  Event instance.
+     *
+     * @return  void
      *
      * @since  4.0.0
      */
-    public function onAjaxSampledataApplyStep5()
+    public function onAjaxSampledataApplyStep5(AjaxEvent $event): void
     {
         if ($this->getApplication()->getInput()->get('type') !== $this->_name) {
             return;
@@ -236,17 +305,19 @@ class PlgSampledataJed extends CMSPlugin
         $response['success'] = true;
         $response['message'] = Text::_('PLG_SAMPLEDATA_JED_STEP5_SUCCESS');
 
-        return $response;
+        $event->addResult($response);
     }
 
     /**
      * Fifth step to enter the sampledata. Contacts
      *
-     * @return  array|void  Will be converted into the JSON response to the module.
+     * @param   AjaxEvent  $event  Event instance.
+     *
+     * @return  void
      *
      * @since  3.8.0
      */
-    public function onAjaxSampledataApplyStep6()
+    public function onAjaxSampledataApplyStep6(AjaxEvent $event): void
     {
         if ($this->getApplication()->getInput()->get('type') !== $this->_name) {
             return;
@@ -258,7 +329,7 @@ class PlgSampledataJed extends CMSPlugin
         $response['success'] = true;
         $response['message'] = Text::_('PLG_SAMPLEDATA_JED_STEP6_SUCCESS');
 
-        return $response;
+        $event->addResult($response);
     }
 
     protected function importFile($file)
